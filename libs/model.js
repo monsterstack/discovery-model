1547ad2b827d24f6e92b3b28a8a74ae588e70c5e
zacharyrote <zachary.rote@gmail.com>
2017-01-13 08:27:52 -0500

    Squash all changes within 5 sec

--- libs/model.js
+++ libs/model.js
@@ -184,7 +184,7 @@ const updateService = (service) => {
 const onServiceChange = (serviceTypes, cb) => {
   let myFeed = ServiceDescriptor.filter((service) => {
     return thinky.r.expr(serviceTypes).contains(service("type"));
-  }).changes();
+  }).changes({squash: 5});
 
   myFeed.then((feed) => {
     feed.each((err, doc) => {
