081341b87e9240746fbd5c12327e3dbbb1e92fa4
zacharyrote <zachary.rote@gmail.com>
2017-01-16 13:04:43 -0500

    fix

--- libs/model.js
+++ libs/model.js
@@ -84,14 +84,25 @@ const countServices = (types, stageFilter, regionFilter) => {
   }
   if(types) {
     console.log("Checking with types");
-    return ServiceDescriptor.filter(filter, (service) => {
-      console.log(thinky.r.expr(types).contains(service("type")));
-      return thinky.r.expr(types).contains(service("type"));
-    }).run().then((services) => {
-      return {
-        count: services.length
-      }
-    });
+    if(filter.hasOwnProperty("stage") || filter.hasOwnProperty("region")) {
+      return ServiceDescriptor.filter(filter, (service) => {
+        console.log(thinky.r.expr(types).contains(service("type")));
+        return thinky.r.expr(types).contains(service("type"));
+      }).run().then((services) => {
+        return {
+          count: services.length
+        }
+      });
+    } else {
+      return ServiceDescriptor.filter((service) => {
+        console.log(thinky.r.expr(types).contains(service("type")));
+        return thinky.r.expr(types).contains(service("type"));
+      }).run().then((services) => {
+        return {
+          count: services.length
+        }
+      });
+    }
   } else {
     return ServiceDescriptor.filter(filter).count().execute().then((count) => {
       return {
